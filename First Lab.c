1d051b3c9e4d9e1a85d169d374e7b4eb4d7f46ee
Kartavya Sharma <kartavya.sharma@hotmail.com>
2025-02-27 18:52:40 -0600

    Update First Lab.c
    I was unaware leetcode did all the stuff such as #include and most of the structures hence why this one is longer.

--- First Lab.c
+++ First Lab.c
@@ -87,26 +87,3 @@ struct ListNode* makList(int length) {
 }
 
 
-// Problem 2
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-struct ListNode* reverseList(struct ListNode* head) {
-    struct ListNode *curr = head, *prev = NULL, *next;
-
-    while (curr != NULL) {
-        next = curr->next;
-        curr->next = prev;
-
-        prev = curr;
-        curr = next;
-    }
-    return prev;
-}
-
-
-
